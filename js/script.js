0779561c588b337081159cb289ad8fe436e6c47c
Julia <jmoraeslia@proton.me>
2023-12-20 22:13:47 -0300

    new section

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let open1 = document.getElementById("open_1")
+let open2 = document.getElementById("open_2")
+let open3 = document.getElementById("open_3")
+let open4 = document.getElementById("open_4")
+
+
+let box1 = document.getElementById("box_1")
+let box2 = document.getElementById("box_2")
+let box3 = document.getElementById("box_3")
+let box4 = document.getElementById("box_4")
+
+open1.addEventListener('click', ()=> {
+    box1.style.display = 'block'
+    box
+})
+open2.addEventListener('click', ()=> {
+    box2.style.display = 'block'
+})
+open3.addEventListener('click', ()=> {
+    box3.style.display = 'block'
+})
+open4.addEventListener('click', ()=> {
+    box4.style.display = 'block'
+})
+
+
+
